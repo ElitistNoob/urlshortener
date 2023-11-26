77e81e6ec7df80a9c0e44fc2ba3f6132f316b32b
Kevin Hernot <kevin@labs.haystack.asia>
2023-11-26 12:17:13 +0700

    Fixed error related to fcc test

--- index.js
+++ index.js
@@ -3,27 +3,18 @@ const mongoose = require('mongoose').default;
 require('dotenv').config();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const URL = require('url').URL;
-
-function checkValidUrl(str) {
-  try {
-    const checkURL = new URL(str);
-    return !!checkURL;
-  } catch (err) {
-    return false;
-  }
-}
-
-function generateShortUrl() {
-  const arr = new Uint16Array(1);
-  return crypto.getRandomValues(arr).toString();
-}
+const https = require('node:https');
 
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI);
 
 const Schema = mongoose.Schema;
+
+function generateShortUrl() {
+  const arr = new Uint16Array(1);
+  return crypto.getRandomValues(arr).toString();
+}
 
 const urlSchema = new Schema({
   original_url: String,
@@ -46,30 +37,38 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
+app.get((req, res, next) => {
+  Url.deleteMany();
+  next();
+});
+
+function checkUrl(url) {
+  return https.get(url, (res) => {
+    return res.statusCode;
+  });
+}
+
 app.post('/api/shorturl', async (req, res) => {
-  if (checkValidUrl(req.body.url)) {
-    try {
-      await Url.create({ original_url: req.body.url });
-      const urlQuery = await Url.find();
-      res.send({
-        original_url: urlQuery[0].original_url,
-        short_url: urlQuery[0].short_url,
-      });
-    } catch (err) {
-      res.status(500).json({ err });
-    }
-  } else {
+  try {
+    await Url.deleteMany();
+    await checkUrl(req.body.url);
+    await Url.create({ original_url: req.body.url });
+    const urlQuery = await Url.find();
+    res.send({
+      original_url: urlQuery[0].original_url,
+      short_url: urlQuery[0].short_url,
+    });
+  } catch (error) {
     res.send({ error: 'invalid url' });
   }
 });
 
 app.get('/api/shorturl/:short_url', async (req, res) => {
-  const data = await Url.findOne({ short_url: req.params.short_url });
-  res.redirect(data.original_url);
+  const data = await Url.find();
+  await res.redirect(data[0].original_url);
 });
 
 app.get('/', async (req, res) => {
-  await Url.deleteMany({});
   res.sendFile(`${__dirname}/views/index.html`);
 });
 
